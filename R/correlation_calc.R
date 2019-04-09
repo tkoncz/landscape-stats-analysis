de74ac326b28e21769ff2fda66c50b2045fe6630
tomiaJO <t.koncz.com>
2019-04-09 21:59:19 +0200

    :construction: color charts based on the difference between frequency and expected frequency in each tile (WIP version)

--- R/correlation_calc.R
+++ R/correlation_calc.R
@@ -25,37 +25,53 @@ createSummaryDtForContingencyPlot <- function(dt, variable1, variable2) {
             get(paste0("frequency_", variable2)), 
             total_rows
         )] %>%
+        .[, frequency_to_expected_frequency := frequency / expected_frequency] %>% 
         .[, c(variable1, variable2, "frequency", "expected_frequency",
-              "frequency_rate", "expected_frequency_rate"), with = FALSE]
+              "frequency_rate", "expected_frequency_rate", 
+              "frequency_to_expected_frequency"), 
+            with = FALSE
+        ]
 }
 
-plotContigencyTable <- function(summary_dt, variable1, variable2) {
+plotContigencyTable <- function(summary_dt, variable1, variable2, show_rates = FALSE) {
     p <- summary_dt %>%
-        ggplot(aes_string(x = variable1, y = variable2, fill = "frequency_rate")) +
-        geom_tile() +
-        geom_text(aes(label = scales::comma(frequency))) +
-        geom_text(
-            aes(label = glue("({scales::comma(round(expected_frequency))})")),
-            vjust = 1.8, color = "red"
-        ) +
+        .[, frequency_to_expected_frequency_capped := pmin(
+            frequency_to_expected_frequency, 2
+        )] %>% 
+        ggplot(aes_string(x = variable1, y = variable2)) +
+        geom_tile(aes(fill = frequency_to_expected_frequency_capped)) +
+        scale_fill_gradientn(colours = rev(terrain.colors(10)))
+
+    if (show_rates == TRUE) {
+        p <- p + 
+            geom_text(aes(label = scales::percent(frequency_rate)), fontface = "bold") +
+            geom_text(
+                aes(label = glue("({scales::percent(expected_frequency_rate)})")),
+                vjust = 1.8, color = "red", fontface = "bold"
+            )
+    } else {
+        p <- p + 
+            geom_text(aes(label = scales::comma(frequency)), fontface = "bold") +
+            geom_text(
+                aes(label = glue("({scales::comma(round(expected_frequency))})")),
+                vjust = 1.8, color = "red", fontface = "bold"
+            )
+    }
+    
+    p <- p +
         labs(
             title = bquote(Contingency~table~between~variables~bold(.(variable1))~and~bold(.(variable2))),
-            subtitle = "Chi-squared: ", ## TODO
-            caption = ""
+            subtitle = "Chi-squared: " ## TODO
         ) +
-        scale_fill_gradient(low = "white", high = "steelblue", guide = FALSE) + ## TODO
-        # scale_colour_manual(
-        #     values = c(frequency = "black", expected_frequency = "red"),
-        #     name = NULL,
-        #     labels = c("frequency", "(expected_frequency)")
-        # ) + 
         theme_minimal() +
         theme(
             legend.position = "bottom"
         )
 
     ggsave(
-        filename = file.path("figures", glue("contingency_table_{variable1}_{variable2}.png")),
+        filename = file.path("figures", glue(
+            "contingency_table_{variable1}_{variable2}_show_rates_is_{show_rates}.png"
+        )),
         plot = p,
         width = 9, height = 9
     )
--- scripts/feature_correlations.R
+++ scripts/feature_correlations.R
@@ -1,11 +1,13 @@
 source("global.R")
 
-ascii <- readData(file.path("data", "ascii"), refetch = FALSE)
-convertVariableColsToFactor_(ascii)
+ascii <- readData(file.path("data", "ascii"), refetch = FALSE) %>% 
+    convertVariableColsToFactor_()
 
-map(combn(names(ascii)[c(-1, -2)], 2, simplify = FALSE)[1], ~{
+variable_combinations <- combn(names(ascii)[c(-1, -2)], 2, simplify = FALSE)
+
+map(variable_combinations, ~{
     createSummaryDtForContingencyPlot(ascii, .x[[1]], .x[[2]]) %>%
-        plotContigencyTable(.x[[1]], .x[[2]])    
+        plotContigencyTable(.x[[1]], .x[[2]], show_rates = TRUE)
 })
-
+    
 ascii[, chisq.test(diffuz, fb_valtozatossag)]
