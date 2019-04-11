953060f26876666c83e67831576c019edff12f9e
tomiaJO <t.koncz.com>
2019-04-11 21:25:35 +0200

    :sparkles: calculate chi-squared and p-values, add them to contigency table plots

--- R/correlation_calc.R
+++ R/correlation_calc.R
@@ -10,71 +10,96 @@ createSummaryDtForContingencyPlot <- function(dt, variable1, variable2) {
     dt %>%
         .[, .(frequency = .N), by = c(variable1, variable2)] %>%
         .[, (paste0("frequency_", variable1)) := sum(frequency), by = variable1] %>%
-        .[, (paste0("frequency_", variable2)) := sum(frequency), by = variable2] %>% 
+        .[, (paste0("frequency_", variable2)) := sum(frequency), by = variable2] %>%
         .[, `:=`(
-            total_rows = sum(frequency), 
+            total_rows = sum(frequency),
             frequency_rate = frequency / sum(frequency)
         )] %>%
         .[, expected_frequency := calculateExpectedFrequency(
-            get(paste0("frequency_", variable1)), 
-            get(paste0("frequency_", variable2)), 
+            get(paste0("frequency_", variable1)),
+            get(paste0("frequency_", variable2)),
             total_rows
         )] %>%
         .[, expected_frequency_rate := calculateExpectedFrequencyRate(
-            get(paste0("frequency_", variable1)), 
-            get(paste0("frequency_", variable2)), 
+            get(paste0("frequency_", variable1)),
+            get(paste0("frequency_", variable2)),
             total_rows
         )] %>%
-        .[, frequency_to_expected_frequency := frequency / expected_frequency] %>% 
+        .[, frequency_to_expected_frequency := frequency / expected_frequency] %>%
         .[, c(variable1, variable2, "frequency", "expected_frequency",
-              "frequency_rate", "expected_frequency_rate", 
-              "frequency_to_expected_frequency"), 
+              "frequency_rate", "expected_frequency_rate",
+              "frequency_to_expected_frequency"),
             with = FALSE
         ]
 }
 
-plotContigencyTable <- function(summary_dt, variable1, variable2, show_rates = FALSE) {
-    p <- summary_dt %>%
+plotContigencyTable <- function(summary_dt,
+                                variable1, variable2,
+                                chi_square_test,
+                                show_rates = FALSE) {
+    contingency_plot <- summary_dt %>%
         .[, frequency_to_expected_frequency_capped := pmin(
             frequency_to_expected_frequency, 2
-        )] %>% 
+        )] %>%
         ggplot(aes_string(x = variable1, y = variable2)) +
         geom_tile(aes(fill = frequency_to_expected_frequency_capped)) +
         scale_fill_gradientn(colours = rev(terrain.colors(10)))
 
     if (show_rates == TRUE) {
-        p <- p + 
+        contingency_plot <- contingency_plot +
             geom_text(aes(label = scales::percent(frequency_rate)), fontface = "bold") +
             geom_text(
                 aes(label = glue("({scales::percent(expected_frequency_rate)})")),
                 vjust = 1.8, color = "red", fontface = "bold"
             )
     } else {
-        p <- p + 
+        contingency_plot <- contingency_plot +
             geom_text(aes(label = scales::comma(frequency)), fontface = "bold") +
             geom_text(
                 aes(label = glue("({scales::comma(round(expected_frequency))})")),
                 vjust = 1.8, color = "red", fontface = "bold"
             )
     }
-    
-    p <- p +
+
+    contingency_plot <- contingency_plot +
         labs(
             title = bquote(Contingency~table~between~variables~bold(.(variable1))~and~bold(.(variable2))),
-            subtitle = "Chi-squared: " ## TODO
+            subtitle = createSubtitleFromChiSquareTest(chi_square_test)
         ) +
         theme_minimal() +
         theme(
-            legend.position = "bottom"
+            legend.position = "bottom",
+            panel.grid = element_blank(),
+            axis.ticks = element_blank()
         )
 
+
+
     ggsave(
-        filename = file.path("figures", glue(
-            "contingency_table_{variable1}_{variable2}_show_rates_is_{show_rates}.png"
-        )),
-        plot = p,
+        filename = file.path(
+            "figures", createFileNameForSaving(variable1, variable2, show_rates)
+        ),
+        plot = contingency_plot,
         width = 9, height = 9
     )
 
-    p
+    contingency_plot
+}
+
+createSubtitleFromChiSquareTest <- function(chi_square_test) {
+    chi_squared <- scales::comma(chi_square_test[["statistic"]])
+    p_value     <- format(chi_square_test[["p.value"]], nsmall = 6)
+
+    glue("Chi-squared: {chi_squared}, p-value: {p_value}")
+}
+
+createFileNameForSaving <- function(variable1, variable2, show_rates) {
+    file_name <- glue("contingency_table_{variable1}_{variable2}")
+    if (show_rates == TRUE) {
+        file_name <- paste0(file_name, "_with_rates.png")
+    } else {
+        file_name <- paste0(file_name, "_with_absolute_numbers.png")
+    }
+
+    file_name
 }
--- scripts/feature_correlations.R
+++ scripts/feature_correlations.R
@@ -1,13 +1,14 @@
 source("global.R")
 
-ascii <- readData(file.path("data", "ascii"), refetch = FALSE) %>% 
+ascii <- readData(file.path("data", "ascii"), refetch = FALSE) %>%
     convertVariableColsToFactor_()
 
 variable_combinations <- combn(names(ascii)[c(-1, -2)], 2, simplify = FALSE)
 
 map(variable_combinations, ~{
-    createSummaryDtForContingencyPlot(ascii, .x[[1]], .x[[2]]) %>%
-        plotContigencyTable(.x[[1]], .x[[2]], show_rates = TRUE)
+    chi_square_test <- chisq.test(ascii[[.x[[1]]]], ascii[[.x[[2]]]])
+    summary_table <- createSummaryDtForContingencyPlot(ascii, .x[[1]], .x[[2]])
+
+    summary_table %>% plotContigencyTable(.x[[1]], .x[[2]], chi_square_test, show_rates = TRUE)
+    summary_table %>% plotContigencyTable(.x[[1]], .x[[2]], chi_square_test, show_rates = FALSE)
 })
-    
-ascii[, chisq.test(diffuz, fb_valtozatossag)]
