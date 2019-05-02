231e21091013b22c1f6069e0607494ccc792a7b3
tkoncz <tamas.koncz@emarsys.com>
2019-05-02 22:43:05 +0200

    :recycle: refactor observed / expected count calculations for contingency table plotting

--- R/correlation_calc.R
+++ R/correlation_calc.R
@@ -1,62 +1,60 @@
-calculateExpectedFrequency <- function(freq1, freq2, total) {
-    freq1 / total * freq2
+getAssociationStatistics <- function(contingency_table, variable1, variable2) {
+    association_statistics <- assocstats(contingency_table)
+
+    data.table(
+        variable1 = variable1,
+        variable2 = variable2,
+        pearson_chi_squared = association_statistics[["chisq_tests"]][2],
+        p_value = association_statistics[["chisq_tests"]][6],
+        pearson_contingency_coeff = association_statistics[["contingency"]],
+        cramers_v = association_statistics[["cramer"]]
+    )
 }
 
-calculateExpectedFrequencyRate <- function(freq1, freq2, total) {
-    freq1 / total * freq2 / total
-}
+createSummaryDtForContingencyPlot <- function(contingency_table, variable1, variable2) {
+    chisq_test <- chisq.test(contingency_table)
+    observed <- chisq_test$observed %>%
+        as.table() %>%
+        as.data.table() %>%
+        setnames(c(variable1, variable2, "observed_count"))
 
-createSummaryDtForContingencyPlot <- function(dt, variable1, variable2) {
-    dt %>%
-        .[, .(frequency = .N), by = c(variable1, variable2)] %>%
-        .[, (paste0("frequency_", variable1)) := sum(frequency), by = variable1] %>%
-        .[, (paste0("frequency_", variable2)) := sum(frequency), by = variable2] %>%
-        .[, `:=`(
-            total_rows = sum(frequency),
-            frequency_rate = frequency / sum(frequency)
-        )] %>%
-        .[, expected_frequency := calculateExpectedFrequency(
-            get(paste0("frequency_", variable1)),
-            get(paste0("frequency_", variable2)),
-            total_rows
-        )] %>%
-        .[, expected_frequency_rate := calculateExpectedFrequencyRate(
-            get(paste0("frequency_", variable1)),
-            get(paste0("frequency_", variable2)),
-            total_rows
-        )] %>%
-        .[, frequency_to_expected_frequency := frequency / expected_frequency] %>%
-        .[, c(variable1, variable2, "frequency", "expected_frequency",
-              "frequency_rate", "expected_frequency_rate",
-              "frequency_to_expected_frequency"),
-            with = FALSE
-        ]
+    expected <- chisq_test$expected %>%
+        as.table() %>%
+        as.data.table() %>%
+        setnames(c(variable1, variable2, "expected_count"))
+
+    summary_dt <- merge(
+        observed, expected,
+        all = TRUE, by = c(variable1, variable2)
+    ) %>%
+        .[, observed_rate := observed_count / sum(observed_count)] %>%
+        .[, expected_rate := expected_count / sum(expected_count)] %>%
+        .[, observed_expected_count_diff := abs(observed_count - expected_count)]
+
+    summary_dt
 }
 
 plotContigencyTable <- function(summary_dt,
                                 variable1, variable2,
                                 association_statistics,
                                 show_rates = FALSE) {
-    contingency_plot <- summary_dt %>%
-        .[, frequency_to_expected_frequency_capped := pmin(
-            frequency_to_expected_frequency, 2
-        )] %>%
+    contingency_plot <- summary_dt  %>%
         ggplot(aes_string(x = variable1, y = variable2)) +
-        geom_tile(aes(fill = frequency_to_expected_frequency_capped)) +
-        scale_fill_gradientn(colours = rev(terrain.colors(10)))
+        geom_tile(aes(fill = observed_expected_count_diff)) +
+        scale_fill_gradient(low = "#ddd6f3", high = "#faaca8")
 
     if (show_rates == TRUE) {
         contingency_plot <- contingency_plot +
-            geom_text(aes(label = scales::percent(frequency_rate)), fontface = "bold") +
+            geom_text(aes(label = scales::percent(observed_rate)), fontface = "bold") +
             geom_text(
-                aes(label = glue("({scales::percent(expected_frequency_rate)})")),
+                aes(label = glue("({scales::percent(expected_rate)})")),
                 vjust = 1.8, color = "red", fontface = "bold"
             )
     } else {
         contingency_plot <- contingency_plot +
-            geom_text(aes(label = scales::comma(frequency)), fontface = "bold") +
+            geom_text(aes(label = scales::comma(observed_count)), fontface = "bold") +
             geom_text(
-                aes(label = glue("({scales::comma(round(expected_frequency))})")),
+                aes(label = glue("({scales::comma(round(expected_count))})")),
                 vjust = 1.8, color = "red", fontface = "bold"
             )
     }
@@ -68,12 +66,10 @@ plotContigencyTable <- function(summary_dt,
         ) +
         theme_minimal() +
         theme(
-            legend.position = "bottom",
+            legend.position = "none",
             panel.grid = element_blank(),
             axis.ticks = element_blank()
         )
-
-
 
     ggsave(
         filename = file.path(
@@ -87,14 +83,14 @@ plotContigencyTable <- function(summary_dt,
 }
 
 createSubtitleFromAssociationStats <- function(association_statistics) {
-    pearson_chi_squared <- scales::comma(association_statistics$chisq_tests[2])
-    pearson_p_value     <- format(association_statistics$chisq_tests[6], nsmall = 3)
-    pearson_contingency_coeff <- round(association_statistics$contingency, digits = 3)
-    cramer_v                  <- round(association_statistics$cramer, digits = 4)
+    pearson_chi_squared       <- scales::comma(association_statistics[["pearson_chi_squared"]])
+    p_value                   <- format(association_statistics[["p_value"]], nsmall = 3)
+    pearson_contingency_coeff <- round(association_statistics[["pearson_contingency_coeff"]], digits = 3)
+    cramers_v                 <- round(association_statistics[["cramers_v"]], digits = 4)
 
     paste(
-        glue("Pearson's X-squared: {pearson_chi_squared}, p-value: {pearson_p_value}"),
-        glue("Pearson's Contingency Coeff.: {pearson_contingency_coeff}, Cramer's V: {cramer_v}"),
+        glue("Pearson's X-squared: {pearson_chi_squared}, p-value: {p_value}"),
+        glue("Pearson's Contingency Coeff.: {pearson_contingency_coeff}, Cramer's V: {cramers_v}"),
         sep = "\n"
     )
 }
--- scripts/feature_correlations.R
+++ scripts/feature_correlations.R
@@ -6,9 +6,14 @@ ascii <- readData(file.path("data", "ascii"), refetch = FALSE) %>%
 variable_combinations <- combn(names(ascii)[c(-1, -2)], 2, simplify = FALSE)
 
 walk(variable_combinations, ~{
-    association_statistics <- assocstats(table(ascii[[.x[[1]]]], ascii[[.x[[2]]]]))
+    contingency_table <- table(ascii[[.x[[1]]]], ascii[[.x[[2]]]], useNA = "always")
+    association_statistics <- getAssociationStatistics(
+        contingency_table, variable1 = .x[[1]], variable2 = .x[[2]]
+    )
 
-    summary_table <- createSummaryDtForContingencyPlot(ascii, .x[[1]], .x[[2]])
+    summary_table <- createSummaryDtForContingencyPlot(
+        contingency_table, variable1 = .x[[1]], variable2 = .x[[2]]
+    )
     plotContigencyTable(summary_table, .x[[1]], .x[[2]], association_statistics, show_rates = TRUE)
     plotContigencyTable(summary_table, .x[[1]], .x[[2]], association_statistics, show_rates = FALSE)
 })
